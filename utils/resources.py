743eeb89901e57f7044f290ec3f7628ddcb051a3
Adam Strojek <adam@strojek.info>
2020-02-10 20:44:16 +0100

    New resource type

--- utils/resources.py
+++ utils/resources.py
@@ -18,3 +18,13 @@ class RetriveResource(abc.ABC, BaseResource):
     async def get(self, request: Request) -> APIResponse:
         result = await self.retrive(request)
         return APIResponse(result)
+
+
+class CreateResource(abc.ABC, BaseResource):
+    @abc.abstractmethod
+    async def create(self, request: Request) -> Any:
+        pass
+
+    async def post(self, request: Request) -> APIResponse:
+        result = await self.create(request)
+        return APIResponse(result, status_code=201)
