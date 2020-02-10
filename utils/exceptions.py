0fed8420806d1e84fd65a8e0ab20497694e4ec93
Adam Strojek <adam@strojek.info>
2020-02-10 20:33:23 +0100

    New exception for auth

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -3,3 +3,10 @@ from starlette.exceptions import HTTPException
 
 class APIException(HTTPException):
     pass
+
+
+class TokenExpired(APIException):
+    status_code = 401
+
+    def __init__(self, detail = None):
+        super().__init__(self.status_code, detail)
