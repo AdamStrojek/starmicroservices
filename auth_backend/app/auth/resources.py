1997731362656738ad0977669884f7c59a48b3eb
Adam Strojek <adam@strojek.info>
2020-02-11 20:00:05 +0100

    New resource for validaing permissions

--- auth_backend/app/auth/resources.py
+++ auth_backend/app/auth/resources.py
@@ -1,17 +1,32 @@
+from typing import Any
 from starlette.requests import Request
 
-from utils.resources import CreateResource
+from utils.resources import CreateResource, RetriveResource
 from utils.auth import get_user_object
-from utils.token import create_token
+from utils.token import create_token, has_permissions
+from utils.exceptions import PermissionError
+from utils.permissions import HasValidToken
 
 
 class CreateTokenResource(CreateResource):
     async def create(self, request: Request) -> Any:
         user = await get_user_object()
         data = {
-            'user': user
+            'user': user,
+            'token_valid': True,
         }
         token = await create_token(data)
         return {
             'token': token
         }
+
+
+class ValidTokenResource(RetriveResource):
+    permissions = [HasValidToken()]
+
+    async def retrive(self, request: Request) -> Any:
+        resource = request.query_params.get('resource', None)
+        if not (await has_permissions(resource, request.state.data['user'])):
+            raise PermissionError(f"You have no permissions for {resource}")
+
+        return {}
--- auth_backend/app/auth/routes.py
+++ auth_backend/app/auth/routes.py
@@ -1,8 +1,9 @@
 from starlette.routing import Route
 
-from .resources import CreateTokenResource
+from .resources import CreateTokenResource, ValidTokenResource
 
 
 routes = [
     Route('/create', endpoint=CreateTokenResource),
+    Route('/valid', endpoint=ValidTokenResource),
 ]
