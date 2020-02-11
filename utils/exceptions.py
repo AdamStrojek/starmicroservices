1ebcde39a05499408c8a74a7687a52ead801fed4
Adam Strojek <adam@strojek.info>
2020-02-11 18:58:37 +0100

    Permissions error exception

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -2,11 +2,15 @@ from starlette.exceptions import HTTPException
 
 
 class APIException(HTTPException):
-    pass
+    status_code = 400
+
+    def __init__(self, detail = None):
+        super().__init__(self.status_code, detail)
 
 
 class TokenExpired(APIException):
     status_code = 401
 
-    def __init__(self, detail = None):
-        super().__init__(self.status_code, detail)
+
+class PermissionError(APIException):
+    status_code = 403
