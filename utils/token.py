02dfaed8a10ed58a9f232edd25753bf33403e809
Adam Strojek <adam@strojek.info>
2020-02-11 19:06:34 +0100

    Fix token expiration timestamp, new has_permission function

--- utils/token.py
+++ utils/token.py
@@ -6,9 +6,10 @@ import ujson
 from app import settings
 from .exceptions import TokenExpired
 
-async def create_token(data, expiration_time: int = 5):
+
+async def create_token(data, expiration_time: int = 60):
     payload = {
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expiration_time),
+        'exp': (datetime.datetime.utcnow() + datetime.timedelta(seconds=expiration_time)).timestamp(),
         'data': data
     }
     result = jwt.encode(payload, str(settings.JWT_SECRET), algorithm='HS256')
@@ -19,6 +20,10 @@ async def retrive_token(token: Union[bytes, str]):
     try:
         result = jwt.decode(token, str(settings.JWT_SECRET), algorithm='HS256')
     except jwt.ExpiredSignatureError as exp:
-        raise TokenExpired("Token expired")
+        raise TokenExpired(f"Token expired: {exp!s}")
     data = result.get('data', None)
     return data
+
+
+async def has_permissions(resource, user):
+    return resource in user.get('permissions', [])
